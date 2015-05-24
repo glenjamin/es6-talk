ca6a78ddf00b8a4167a81e28c2d15dc7fcefa75d
Glen Mailer <glenjamin@gmail.com>
2015-05-24 21:56:09 +0100

    outline & repl hack

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # es6-talk
+
 A talk about ES6 and beyond
+
+## Overview
+
+* ES3, ES4, ES5
+* CoffeeScript
+* Harmony
+* TC39
+* ES2015
+* ES7+
+* Compat Table
+* Babel
+* Examples
+  - arrows
+  - default params
+  - destructuring
+  - spread
+  - object shorthands
+  - computed properties
+  - template strings
+  - generators
+  - Map/Set/WeakMap/Symbol
+  - iterator protocol
+  - object spread (ES7)
+* Other niceties
+  - iteration order specified
+  - Proxy
+* Stuff I don't really care about (sorry)
+  - class
+  - decorators (ES7)
+  - let / const
+  - for..of
+
---
+++ babel-repl.js
@@ -0,0 +1,39 @@
+show("Hax");
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+Paste the following snippet into the
+babel repl to use it for the presentation
+*/
+/*eslint-disable */
+var out;
+function init() {
+  var windows = document.getElementsByClassName('babel-repl-editor');
+  windows[0].style.top = windows[1].style.top = 0;
+  var toggle = document.getElementsByClassName('navbar-toggle')[0];
+  if (toggle) toggle.parentNode.removeChild(toggle);
+  out = document.getElementsByClassName('navbar-brand')[0];
+  out.classList.remove('logo');
+  out.style.float = 'none';
+  out.style.display = 'block';
+  out.style.textAlign = 'center';
+  init = init = ()=>0;
+}
+function show(...data) {
+  init();
+  out.textContent = data
+    .map((x) => JSON.stringify(x, null, ' '))
+    .join(' \u00A0 ');
+}
+init();
